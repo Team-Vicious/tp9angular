21245fdf4d9480f6ea9e17b060a494369d45f3c0
sebastianmauriz@gmail.com <sebastianmauriz@gmail.com>
2021-06-04 17:53:48 -0300

    agrego componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { InstrumentosComponent } from './components/instrumentos/instrumentos.component';
+import { UbicacionComponent } from './components/ubicacion/ubicacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: 'home', component: HomeComponent },
+    { path: 'instrumentos', component: InstrumentosComponent },
+    { path: 'donde-estamos', component: UbicacionComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { InstrumentosComponent } from './components/instrumentos/instrumentos.component';
+import { UbicacionComponent } from './components/ubicacion/ubicacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    InstrumentosComponent,
+    UbicacionComponent
   ],
   imports: [
     BrowserModule,
